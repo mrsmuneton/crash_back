5f41c3ef8ee9c21c693c0f39aae8376cb860918b
Ray <mrs.muneton@gmail.com>
2019-05-11 23:12:54 -0700

    setup.sh including tmux and mix deps.get

--- server-setup.sh
+++ server-setup.sh
@@ -1,5 +1,6 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
-sudo apt-get install esl-erlang elixir git lsof -y
+sudo apt-get install esl-erlang elixir git tmux lsof -y
 git clone https://github.com/mrsmuneton/crash_back.git
 cd crash_back
+mix deps.get

36589e065bf487a03adf1aecb11486f64b37b447
Ray <mrs.muneton@gmail.com>
2019-05-11 19:31:31 -0700

    Adding resource routes

--- lib/crash_back_web/router.ex
+++ lib/crash_back_web/router.ex
@@ -17,6 +17,9 @@ defmodule CrashBackWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    resources "/info", InfoController
+    resources "/item", ItemController
   end
 
   # Other scopes may use custom stacks.

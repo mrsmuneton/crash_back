5231d329d04b182e79c9046d45527f0ca551d0a9
Ray <mrs.muneton@gmail.com>
2019-05-11 19:29:45 -0700

    Modified server-setup

--- server-setup.sh
+++ server-setup.sh
@@ -1,6 +1,5 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
-sudo apt-get install git
-sudo apt-get install lsof
+sudo apt-get install esl-erlang elixir git lsof -y
+git clone https://github.com/mrsmuneton/crash_back.git
+cd crash_back
